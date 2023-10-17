f01b828e895232a7d423fd28421c8e7760c7498c
JohnnyBeProgramming <johnathan.lodder@gmail.com>
2023-10-17 18:07:02 +0200

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 from distutils.core import setup
 
 name = 'anyserver'
-version = '0.2.9'
+version = '0.3.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
